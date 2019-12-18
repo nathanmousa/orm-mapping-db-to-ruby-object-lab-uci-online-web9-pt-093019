90e8e60e5da40222184764ae065d00ff6b3a9b4c
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 17:34:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
       WHERE grade = 10
       LIMIT 1
       SQL
-    DB[:conn].execute(sql, limit).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
@@ -83,7 +83,7 @@ class Student
       FROM students
       WHERE grade = ?
       SQL
-    DB[:conn].execute(sql, limit).map do |row|
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
     end
   end
