304d7823490991955dcb517db47d841932e6bff0
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 17:25:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
+      SELECT * FROM students WHERE name = ? LIMIT 1
       SQL
   end
   
