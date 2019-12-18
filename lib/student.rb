aaf5c3c104ca6270b48898c101e8d8b26037a014
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-18 17:29:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,10 @@ class Student
   def self.all
     sql = <<-SQL
       SELECT *
-      FROM
+      FROM students
       SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
   end
 
   def self.find_by_name(name)
